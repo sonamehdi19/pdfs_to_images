df000b7ff3d42e678f3f85c731311b3e652630e0
Sona <sona.mehdizade0101@gmail.com>
2022-07-07 11:04:39 +0400

    added cli parser

--- conversion.py
+++ conversion.py
@@ -4,15 +4,20 @@ import os
 import os
 import subprocess
 from datetime import datetime
+import argparse
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--input', help='input directory', required=True)
+parser.add_argument('--dest', help='destination directory', required=True)
+args = parser.parse_args()
 
-pdf_dir = r"/Users/sonamehdizade/Desktop/Dataset/batch2/"
-save_path = r'/Users/sonamehdizade/Desktop/Dataset/batch2/batch2_images/'
-os.chdir(pdf_dir)
+#pdf_dir = r"/Users/sonamehdizade/Desktop/Dataset/batch7/"
+#save_path = r'/Users/sonamehdizade/Desktop/Dataset/batch7/batch7_images/'
+# os.chdir(pdf_dir)
 
-for pdf_file in glob.glob(os.path.join(pdf_dir, "*.pdf")):
+for pdf_file in glob.glob(os.path.join(args.input, "*.pdf")):
     pages = convert_from_path(pdf_file, 500)
     for page in pages:
         c_time = datetime.now().strftime("%Y_%m_%d_%H_%M_%S_%f")
-        completeName = os. path. join(save_path, "out_" + c_time + ".jpg")
+        completeName = os. path. join(args.dest, "out_" + c_time + ".jpg")
         page.save(completeName, 'JPEG')
